2b6d5ff7013ac85761d8711b3b4715a7ef14b3cf
jiipfox <79690425+jiipfox@users.noreply.github.com>
2021-09-27 20:54:38 +0300

    w3 w3

--- src/index.js
+++ src/index.js
@@ -9,5 +9,46 @@ if (document.readyState !== "loading") {
 }
 
 function initializeCode() {
-  document.getElementById("app").innerHTML = "<h1>Holoa!</h1>";
+  //document.getElementById("app").innerHTML = "<h1>Holoa!</h1>";
+  addElement("H1", "Para1");
+  addElement("H2", "Para2");
+  addElement("H3", "Para3");
 }
+
+function addElement(h1t, pt, imgsrc) {
+  const newDiv = document.createElement("div");
+  newDiv.setAttribute("class", "wiki-item");
+
+  const header = document.createElement("h1");
+  header.setAttribute("class", "wiki-header");
+  header.textContent = h1t;
+
+  const para = document.createElement("P");
+  para.setAttribute("class", "wiki-text");
+  para.textContent = pt;
+
+  const img = document.createElement("img");
+  img.setAttribute("class", "wiki-img");
+  img.src = imgsrc;
+
+  newDiv.appendChild(header);
+  newDiv.appendChild(para);
+  newDiv.appendChild(img);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("container");
+  document.body.insertBefore(newDiv, currentDiv);
+}
+/**
+  <div class="" >
+  <h1 class="wiki-header">Breed X</h1>
+  <div class="wiki-content">
+    <p class="wiki-text">
+      Some text about this breed.
+    </p>
+    <div class="img-container">
+      <img class="wiki-img" src="">
+    </div>
+  </div>
+</div> 
+*/
