0a88f400138b1bfc24fd9c633cbe48e14ef38250
jiipfox <79690425+jiipfox@users.noreply.github.com>
2021-09-30 15:14:23 +0300

    222 222

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ if (document.readyState !== "loading") {
 async function initializeCode() {
   //document.getElementById("app").innerHTML = "<h1>Holoa!</h1>";
   const addr = "https://dog.ceo/api/breed/haukku/images/random";
+  const summAddr = "https://en.wikipedia.org/api/rest_v1/page/summary/";
+
   const b1 = "Hound";
   const b2 = "Puggle";
   const b3 = "Retriever";
@@ -23,25 +25,30 @@ async function initializeCode() {
   let b4Addr = createSuperAddress(addr, b4);
   let b5Addr = createSuperAddress(addr, b5);
 
+  let summary = await getDogeSummary(b1, summAddr);
   let dogJson = await whoLetTheDogsOut(b1Addr);
   let srcOfImg = parseImgSrc(dogJson);
-  addElement(b1, "Para1", srcOfImg);
+  addElement(b1, summary, srcOfImg);
 
+  summary = await getDogeSummary(b2, summAddr);
   dogJson = await whoLetTheDogsOut(b2Addr);
   srcOfImg = parseImgSrc(dogJson);
-  addElement(b2, "Para2", srcOfImg);
+  addElement(b2, summary, srcOfImg);
 
+  summary = await getDogeSummary(b3, summAddr);
   dogJson = await whoLetTheDogsOut(b3Addr);
   srcOfImg = parseImgSrc(dogJson);
-  addElement(b3, "Para3", srcOfImg);
+  addElement(b3, summary, srcOfImg);
 
+  summary = await getDogeSummary(b4, summAddr);
   dogJson = await whoLetTheDogsOut(b4Addr);
   srcOfImg = parseImgSrc(dogJson);
-  addElement(b4, "Para4", srcOfImg);
+  addElement(b4, summary, srcOfImg);
 
+  summary = await getDogeSummary(b5, summAddr);
   dogJson = await whoLetTheDogsOut(b5Addr);
   srcOfImg = parseImgSrc(dogJson);
-  addElement(b5, "Para5", srcOfImg);
+  addElement(b5, summary, srcOfImg);
 }
 
 function addElement(h1t, pt, imgsrc) {
@@ -85,6 +92,15 @@ async function whoLetTheDogsOut(addr) {
   let response = await fetch(addr);
   let dogs = await response.json();
   return dogs;
+}
+
+async function getDogeSummary(breed, addr) {
+  let summary = "jes";
+  let url = addr + breed;
+  let jsonMsg = await fetch(url);
+  summary = await jsonMsg.json();
+  summary = summary.extract;
+  return summary;
 }
 
 function parseImgSrc(givenJson) {
